eec73f3064325c3bb0d823690176a1947753592c
J-Cuadrado <71754039+J-Cuadrado@users.noreply.github.com>
2023-07-10 15:37:33 +0200

    Update readme.py

--- mvsec_dataset/readme.py
+++ mvsec_dataset/readme.py
@@ -1,1 +1,3 @@
-Please place all unzipped files from the MVSEC dataset on '/data/dataset/MVSEC'.
+Please place all unzipped files from the MVSEC dataset on 'files/raw'.
+
+Generated files will be stored in 'files/saved'.
